b0f59b3c49ddec560ff7e85ef182429721b93d81
mwmcclure7 <mwmcclure7@gmail.com>
2024-11-01 15:56:37 -0400

    Created router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header'
+import Home from './pages/Home'
+import Courses from './pages/Courses'
+import Clubs from './pages/Clubs'
+import Recommendations from './pages/Recommendations'
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/clubs" element={<Clubs />} />
+        <Route path="/recommendations" element={<Recommendations />} />
+      </Routes>
+    </Router>
+  )
 }
 
-export default App
+export default App;
