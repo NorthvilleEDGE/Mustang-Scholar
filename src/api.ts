4e1b0c9fe45185dd88ca933c766e09842c1eb547
mwmcclure7 <mwmcclure7@gmail.com>
2024-12-14 18:49:53 -0500

    Added data retrieval from google sheets

--- src/api.ts
+++ src/api.ts
@@ -1,22 +1,14 @@
 import axios from 'axios';
 
-const SHEETY_API_URL = process.env.SHEETY_API_URL;
+const CLUBS_SHEETY_API_URL = import.meta.env.CLUBS_SHEETY_API_URL;
 
-async function getFirstRow() {
-    try {
-        const response = await axios.get(`${SHEETY_API_URL}/sheet1`);
-        const rows = response.data.sheet1;
-        if (rows.length > 0) {
-            console.log('First row:', rows[0]);
-            return rows[0];
-        } else {
-            console.log('No data found.');
-            return null;
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return null;
-    }
+async function getTextFromFile() {
+    fetch(CLUBS_SHEETY_API_URL)
+    .then((response) => response.json())
+    .then(json => {
+    console.log(json.clubs);
+});
+
 }
 
-getFirstRow();
+export { getTextFromFile };
--- src/pages/Testing.tsx
+++ src/pages/Testing.tsx
@@ -1,10 +1,48 @@
+import { useEffect, useState } from 'react';
+
+const CLUBS_SHEETY_API_URL = import.meta.env.VITE_CLUBS_SHEETY_API_URL;
+
+interface Club {
+    name: string;
+    description: string;
+}
+
 function Testing() {
+    const [clubs, setClubs] = useState<Club[]>([]);
+
+    useEffect(() => {
+        fetch(CLUBS_SHEETY_API_URL)
+        .then((response) => response.json())
+        .then(json => {
+            setClubs(json.clubs);
+            console.log(json);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+    }, []);
+
     return (
         <div>
-            <h1>Testing</h1>
-            <p>Testing 123</p>
+            <h1>Clubs</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {clubs.map((club, index) => (
+                        <tr key={index}>
+                            <td>{club.name}</td>
+                            <td>{club.description}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default Testing;
