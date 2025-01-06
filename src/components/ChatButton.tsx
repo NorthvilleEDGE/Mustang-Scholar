f1c7fd0117abe6d55f07135d7cb5e1b5ca6ddb7c
mwmcclure7 <mwmcclure7@gmail.com>
2025-01-06 09:59:01 -0500

    Removed import React statements

--- src/components/ChatButton.tsx
+++ src/components/ChatButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/ChatButtonStyle.css';
 
 interface ChatButtonProps {
--- src/components/TypingIndicator.tsx
+++ src/components/TypingIndicator.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import '../styles/TypingIndicator.css';
 
 function TypingIndicator() {
